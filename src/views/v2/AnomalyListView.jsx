ff24b0be9b8894957c58c83a20098bca04437217
rytsad1 <rytissadz@gmail.com>
2026-05-21 22:12:09 +0300

    Anomalijos

--- src/views/v2/AnomalyListView.jsx
+++ src/views/v2/AnomalyListView.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from '../../axios';
 import { Link } from 'react-router-dom';
-import NavBar from '../../components/NavBar';
+import NavBar from '../../components/navBar';
 
 const severityClass = {
     low: 'secondary',
